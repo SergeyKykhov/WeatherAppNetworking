44406d7e320536c5246b26f286dd5ffffb3d98ff
Sergey Kykhov <ghrle@mail.ru>
2023-07-24 17:00:43 +0300

    Added the full URL and wrote everything into constants

---
+++ WeatherAppNetworking/NetworkManager/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  WeatherAppNetworking
+//
+//  Created by Sergey Kykhov on 24.07.2023.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let url = "https://api.openweathermap.org/data/2.5/weather"
+    static let paramCoordinates = "?lat=59.57&lon=30.19&appid=\(Constants.API_KEY)"
+    static let API_KEY = "c517f261fefb1276ee794306f6a7031e"
+}
+
