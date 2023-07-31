c115e2d3354548159b84dd5115bae265c56a7f72
Sergey Kykhov <ghrle@mail.ru>
2023-07-31 19:27:32 +0300

    feat: added parameters for URL
    added barometers for one more city, for the implementation of transitions between cities, and getting parameters through the API

--- WeatherAppNetworking/NetworkManager/Constants.swift
+++ WeatherAppNetworking/NetworkManager/Constants.swift
@@ -10,7 +10,9 @@ import UIKit
 
 struct Constants {
     static let url = "https://api.openweathermap.org/data/2.5/weather"
-    static let paramCoordinates = "?lat=59.57&lon=30.19&appid=\(Constants.API_KEY)"
+    static let paramSpb = "?lat=59.57&lon=30.19&appid=\(Constants.API_KEY)"
+    static let paramMoscow = "?lat=55.75&lon=37.61&appid=\(Constants.API_KEY)"
+
     static let API_KEY = "c517f261fefb1276ee794306f6a7031e"
 }
 
